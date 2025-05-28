e4b592e0bb5fa313c7fe9846ff31fade9f716997
Brixx200 <kausal64626@students.apsb.org>
2025-05-28 16:48:24 +0000

    almost done game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,73 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
+   createPlatform(10,300,100,10,'hotPink');
+   createPlatform(250,200,100,10,'hotPink');
+   createPlatform(1100,200,50,10,"hotPink");
+   createPlatform(1300,350,150,10,"hotPink");
+   createPlatform(1375,601,100,10,"lightBlue");
+   createPlatform(350,600,100,10,"hotPink");
+   createPlatform(850,500,100,10,"hotPink");
+   createPlatform(1100,601,50,10,"hotPink");
+   
+   //top walls//
+   createPlatform(450,18,100,10,"hotPink");
+   createPlatform(550,18,100,10,"hotPink");
+   createPlatform(650,18,100,10,"hotPink");
+   createPlatform(750,18,100,10,"hotPink");
+   createPlatform(850,18,10,200,"hotPink")
+   createPlatform(850,215,75,10,"hotPink")
+   //bottom walls//
+   createPlatform(450,120,100,10,"hotPink");
+   createPlatform(550,120,100,10,"hotPink");
+   createPlatform(650,120,100,10,"hotPink");
+   createPlatform(750,120,10,200,"hotPink");
+   createPlatform(750,320,200,10,"hotPink");
+   //hidden collectable wall//
+   createPlatform(1300,350,10,300,"hotPink");
+   //grass//
+   createPlatform(100,700,10,50,"green");
+   createPlatform(125,700,10,50,"green");
+   createPlatform(150,700,10,50,"green");
+   createPlatform(200,700,10,50,"green");
+   createPlatform(225,700,10,50,"green");
+   createPlatform(250,700,10,50,"green");
+   createPlatform(300,700,10,50,"green");
+   createPlatform(325,700,10,50,"green");
+   createPlatform(350,700,10,50,"green");
+   createPlatform(400,700,10,50,"green");
+   createPlatform(425,700,10,50,"green");
+   createPlatform(450,700,10,50,"green");
+   createPlatform(500,700,10,50,"green");
+   createPlatform(525,700,10,50,"green");
+   createPlatform(550,700,10,50,"green");
+   createPlatform(600,700,10,50,"green");
+   createPlatform(625,700,10,50,"green");
+   createPlatform(650,700,10,50,"green");
+   createPlatform(700,700,10,50,"green");
+   createPlatform(725,700,10,50,"green");
+   createPlatform(750,700,10,50,"green");
+   
 
 
 
     // TODO 3 - Create Collectables
 
-
-
+    createCollectable("diamond",810,50);
+    createCollectable("grace",375,550);
+    createCollectable("kennedi",875,450);
+    createCollectable('steve',1335,375);
+    createCollectable('max',1105,225);
     
     // TODO 4 - Create Cannons
 
+    createCannon("left",300,2000);
+    createCannon('bottom',1075,750,);
+    createCannon('top',250,1000,);
 
     
     
